2da038a2eb7ac0d4c74121733f4aabf24b01e198
unknown <ef3d88@gmail.com>
2021-06-01 19:37:55 +0700

    concurrency

--- src/sample/Foo.java
+++ src/sample/Foo.java
@@ -1,9 +1,113 @@
 package sample;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Foo {
+    static AtomicInteger flag = new AtomicInteger(0);
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        printFirst.run();
+        System.out.println(flag.get());
+        flag.incrementAndGet();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        while (flag.get() != 1) ;
+        printSecond.run();
+        System.out.println(flag.get());
+        flag.incrementAndGet();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        while (flag.get() != 2) ;
+        printThird.run();
+        System.out.println(flag.get());
+    }
+}
+
+
+
+/*
+*******************REENTRANLOCK()**********************
+    int flag = 0;
+
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        lock.lock();
+        printFirst.run();
+        flag++;
+        condition.signalAll();
+        lock.unlock();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        lock.lock();
+        while (flag != 1) {
+            condition.await();
+        }
+        printSecond.run();
+        flag++;
+        condition.signalAll();
+        lock.unlock();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        lock.lock();
+        while (flag != 2) {
+            condition.await();
+        }
+        printThird.run();
+        lock.unlock();
+    }
+}
+*/
+
+
+
+
+
+/*
+******************synchronized************************
+    int flag = 0;
+
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+        while (flag != 0) {
+            wait();
+        }
+        printFirst.run();
+        flag++;
+        notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while (flag != 1) {
+            wait();
+        }
+        printSecond.run();
+        flag++;
+        notifyAll();
+    }
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while (flag != 2) {
+            wait();
+        }
+        printThird.run();
+        flag++;
+    }
+
+ */
+
+
+/*
+**********************CDL***********************
 
     CountDownLatch cdl = new CountDownLatch(1);
     CountDownLatch cdl2 = new CountDownLatch(2);
@@ -24,14 +128,12 @@ public class Foo {
         cdl2.await();
         printThird.run();
     }
-
-
-
-
+*/
 
 
 /*
 ***********************SEMAPHORE************************************
+
         Semaphore semaphore = new Semaphore(0);
         Semaphore semaphore2 = new Semaphore(0);
 
@@ -50,5 +152,6 @@ public class Foo {
         semaphore2.acquire();
         printThird.run();
         semaphore2.release();
-    }*/
-}
+    }
+*/
+
