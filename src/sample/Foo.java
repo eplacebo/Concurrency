332e62c1444b7afa325226d598974fc3a22b79f8
unknown <ef3d88@gmail.com>
2021-06-01 12:19:43 +0700

    cdl

--- src/sample/Foo.java
+++ src/sample/Foo.java
@@ -1,9 +1,37 @@
 package sample;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 
 public class Foo {
 
+    CountDownLatch cdl = new CountDownLatch(1);
+    CountDownLatch cdl2 = new CountDownLatch(2);
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        printFirst.run();
+        cdl.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        cdl.await();
+        printSecond.run();
+        cdl2.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        cdl2.countDown();
+        cdl2.await();
+        printThird.run();
+    }
+
+
+
+
+
+
+/*
+***********************SEMAPHORE************************************
         Semaphore semaphore = new Semaphore(0);
         Semaphore semaphore2 = new Semaphore(0);
 
@@ -22,5 +50,5 @@ public class Foo {
         semaphore2.acquire();
         printThird.run();
         semaphore2.release();
-    }
+    }*/
 }
